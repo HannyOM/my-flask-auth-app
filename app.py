8193d5672ed0abcd82b51d59e476cae38800a611
HannyOM <hanny0568@gmail.com>
2025-10-20 03:58:34 +0100

    updated user class and register view

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    
-with app.app_context():
-        db.create_all()
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register")
-def register():
-    # User types their details and clicks submit 
-    # when submit btn is clicked:
-    # username and password are added to database
-    # 
+@app.route("/register", methods=["GET", "POST"])
+def register():          
+    if request.method == "POST":
+        # Getting the user details and adding them to the database
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username and password:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            return "Registration successful!"  # You might want to redirect to login page instead
     return render_template("register.html")
 
 @app.route("/login")
