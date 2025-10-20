a4502c544c099e96da14afff93914723282a3d78
HannyOM <hanny0568@gmail.com>
2025-10-20 12:18:34 +0100

    Addedd username checking and password hashing functionalities in register view

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,10 +30,15 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         if username and password:
-            new_user = User(username=username, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            return "Registration successful!"  # You might want to redirect to login page instead
+            existing_user = User.query.filter_by(username=username).first()
+            if existing_user:
+                return render_template("username_already_taken.html")
+            else:    
+                password_hash = bcrypt.generate_password_hash('password')
+                new_user = User(username=username, password=password_hash)
+                db.session.add(new_user)
+                db.session.commit()
+                return redirect(url_for("login")) 
     return render_template("register.html")
 
 @app.route("/login")
@@ -37,4 +46,7 @@ def login():
     return render_template("login.html")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
